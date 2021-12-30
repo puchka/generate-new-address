c8ffe80a8b12bafb3e13e1552ae4662b6d2be735
Marius A. Rabenarivo <mariusrabenarivo@gmail.com>
2021-12-30 16:28:13 +0400

    Rename desc to pub_key

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -8,12 +8,12 @@ use bdk::bitcoin::Network;
 use bdk::wallet::AddressInfo;
 use bdk::wallet::AddressIndex::New;
 
-fn get_new_address(desc: String) -> Result<AddressInfo, bdk::Error> {
+fn get_new_address(pub_key: String) -> Result<AddressInfo, bdk::Error> {
     let mut external_descriptor = "wpkh(".to_owned();
-    external_descriptor.push_str(desc.as_str());
+    external_descriptor.push_str(pub_key.as_str());
     external_descriptor.push_str("/84'/0'/0'/0/*)");
     let mut internal_descriptor = String::from("wpkh(").to_owned();
-    internal_descriptor.push_str(desc.as_str());
+    internal_descriptor.push_str(pub_key.as_str());
     internal_descriptor.push_str("/84'/0'/0'/1/*)");
     
     let wallet = Wallet::new_offline(
@@ -29,10 +29,10 @@ fn get_new_address(desc: String) -> Result<AddressInfo, bdk::Error> {
 #[no_mangle]
 pub extern "system" fn Java_GenerateNewAddress_getNewAddress(env: JNIEnv,
                                                              _class: JClass,
-                                                             desc: JString)
+                                                             pub_key: JString)
                                                              -> jstring {
     let desc: String =
-        env.get_string(desc).expect("Couldn't get java string!").into();
+        env.get_string(pub_key).expect("Couldn't get java string!").into();
     
     let output = env.new_string(format!("Generated Address: {}",
                                         get_new_address(desc).unwrap()))
